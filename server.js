2e0a62d373ab39bc09762fcca2eb6d30fb02efba
herbeylhvb-lab <herbeylhvb@gmail.com>
2026-03-13 15:37:32 -0500

    fix: add fs require for db-upload endpoint

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors    = require('cors');
 const path    = require('path');
+const fs      = require('fs');
 const session = require('express-session');
 const helmet  = require('helmet');
 const rateLimit = require('express-rate-limit');
